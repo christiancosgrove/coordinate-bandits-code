38b50e446c567e71f488286abc02f39f18c6341b
Christian Cosgrove <ccosgro2@jhu.edu>
2019-10-30 13:00:43 -0400

    loading data

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ DATA_DIR='./data'
 
 data_files = os.listdir(DATA_DIR)
 
-# for 
+f = data_files[0]
+print('Filename ', f)
+
+
+print('Getting data')
+
+X, y = load_svmlight_file(os.path.join(DATA_DIR, f))
+
+print('X shape ', X.shape)
+print('Y shape ', y.shape)
