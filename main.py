a0b5673ce9a668e12f42adc9f019d70561640d66
Christian Cosgrove <ccosgro2@jhu.edu>
2019-11-21 09:53:19 -0500

    plot max_r and random

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#%%
 from sklearn.datasets import load_svmlight_file
 import os
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+#%%
 DATA_DIR='./data'
 
 data_files = os.listdir(DATA_DIR)
@@ -22,8 +26,7 @@ print('Y shape ', y.shape)
 print('y min', y.min(), ' max ', y.max())
 print('X mean ', np.mean(X))#, ' std', np.std(X))
 print('y min', ((y+1)/2).min(), ' max ', ((y+1)/2).max())
-
-W = np.random.normal(size=X.shape[1], loc=0, scale=1)
+#%%
 def loss_logistic(X, y, W, lam):
     pred = sigmoid(W.T @ X.T)
     yp = (y+1)/2
@@ -40,6 +43,7 @@ def sigmoid(x):
 def shrink(x, lam):
     return np.sign(x) * np.maximum(np.abs(x) - lam, 0)
 
+# partial derivative of f(Ax) wrt x_i
 def partial(W, i, X, y):
     p = -((y+1)/2-sigmoid(W.T @ X.T)) 
     # print('partal shape ', p.shape)
@@ -61,6 +65,7 @@ def res(i, W, X, y, lam, B):
     part = partial(W, i, X, y)
     return np.abs(W[i] - B * shrink(part, lam))
 
+# r_i as defined in the paper
 def reward(i, W, X, y, lam, B):
     beta = 1
     Gi = gap(i, W, X, y, lam, B)
@@ -75,23 +80,57 @@ def reward(i, W, X, y, lam, B):
 lam = 0.01
 vals = []
 coords_chosen = []
-W_init = np.array(W, copy=True)
+W_init = np.random.normal(size=X.shape[1], loc=0, scale=1)
 
-NUM_ITERATIONS = 10000
-for j in tqdm(range(NUM_ITERATIONS)):
-    # Use the 'Lipschitzing' trick - we know that the iterates are contained in ball of radius B
-    if j == 0:
-        B = loss_logistic(X, y, W, lam) / lam
-    # if j % 10 == 0:
-    i = np.random.randint(W.shape[0])
-    # i = j % W.shape[0]
-    # l_before = loss_logistic(X, y, W, lam)
-    print('GAP i ', gap(i, W, X, y, lam, B), 'residual ', res(i, W, X, y, lam, B), 'reward ', reward(i, W, X, y, lam, B))
-    coords_chosen.append(i)
-    update_coord(W, i, X, y, lam)
-    l_after = loss_logistic(X, y, W, lam)
-    print(l_after)
+NUM_ITERATIONS = 50
+def greedy_coordinate(W_init):
+    W = np.array(W_init, copy=True)
+    losses_greedy=[]
+    for j in tqdm(range(NUM_ITERATIONS)):
+        # Use the 'Lipschitzing' trick - we know that the iterates are contained in ball of radius B
+        if j == 0:
+            B = loss_logistic(X, y, W, lam) / lam
+        # if j % 10 == 0:
 
+        rewards = []
+        for i in range(W.shape[0]):
+            # print('GAP i ', gap(i, W, X, y, lam, B), 'residual ', res(i, W, X, y, lam, B), 'reward ', reward(i, W, X, y, lam, B))
+            rewards.append(reward(i, W, X, y, lam, B))
+
+
+        i = np.argmax(rewards)
+        # i = np.random.randint(W.shape[0])
+        # i = j % W.shape[0]
+        # l_before = loss_logistic(X, y, W, lam)
+        coords_chosen.append(i)
+        update_coord(W, i, X, y, lam)
+        l_after = loss_logistic(X, y, W, lam)
+        losses_greedy.append(l_after)
+        # print(l_after)
+    return losses_greedy
+
+def random_coordinate(W_init):
+    W = np.array(W_init, copy=True)
+    losses_random=[]
+    for j in tqdm(range(NUM_ITERATIONS)):
+        # Use the 'Lipschitzing' trick - we know that the iterates are contained in ball of radius B
+        if j == 0:
+            B = loss_logistic(X, y, W, lam) / lam
+        i = np.random.randint(W.shape[0])
+        coords_chosen.append(i)
+        update_coord(W, i, X, y, lam)
+        l_after = loss_logistic(X, y, W, lam)
+        losses_random.append(l_after)
+        # print(l_after)
+    return losses_random
+
+#%%
+plt.plot(greedy_coordinate(W_init))
+plt.plot(random_coordinate(W_init))
+plt.legend(['greedy','random'])
+plt.xlabel('iterations')
+plt.ylabel('Logistic loss')
+plt.show()
 # print('Result', loss_logistic(X, y, W, lam))
 # for _ in range(500):
 #     W = np.array(W_init, copy=True)
@@ -100,3 +139,5 @@ for j in tqdm(range(NUM_ITERATIONS)):
 #         i = coords_chosen[j]
 #         update_coord(W, i, X, y, lam)
 #     print('Result2', loss_logistic(X, y, W, lam))
+
+# %%
